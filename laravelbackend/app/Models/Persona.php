cb23303ceea2394ac48879057fa233cbdf7ea388
guilmarbautista <91688496+guilmarbautista@users.noreply.github.com>
2022-10-18 15:22:54 -0400

    Create Persona.php

---
+++ laravelbackend/app/Models/Persona.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    use HasFactory;
+    protected $table= 'personas';
+    protected $fillable= [
+        'nombre',
+        'apellido',
+        'ci',
+        'sexo',
+        'fechaNacimiento',
+        'nacionalidad',
+
+    ];
+}
