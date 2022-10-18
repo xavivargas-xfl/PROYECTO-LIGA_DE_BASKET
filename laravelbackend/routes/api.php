682ee1e47ef8ac16468070656e06f5afcadbdf42
guilmarbautista <91688496+guilmarbautista@users.noreply.github.com>
2022-10-18 15:22:49 -0400

    Update api.php

--- laravelbackend/routes/api.php
+++ laravelbackend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\API\JugadorController;
+use App\Http\Controllers\API\RegistroController;
+use App\Http\Controllers\API\JuezController;
+use App\Http\Controller\API\PersonaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,34 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('/pre-registro', [RegistroController::class,'store']);
+Route::group(['Middleware'=>'api'],function(){
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    //---------rutas jugador
+    Route::get('/index/{id}', [JugadorController::class,'index']);
+    Route::post('/add-jugador', [JugadorController::class,'store']);
+    Route::get('/listar-jugador',[JugadorController::class,'show']);
+    Route::put('/editar-jugador/{id}', [JugadorController::class,'update']);
+    Route::delete('/eliminar-jugador/{id}', [JugadorController::class,'destroy']);
+
+    //---------rutas juez
+    Route::get('/index', [JuezController::class,'index']);
+    Route::post('/add-juez', [JuezController::class,'store']);
+    Route::get('/listar-juez/{id}', [JuezController::class,'show']);
+    Route::put('/editar-juez/{id}', [JuezController::class,'update']);
+    Route::delete('/eliminar-juez/{id}', [JuezController::class,'destroy']);
+
+     //--------ruta personas
+     
+
+});
+
+    Route::get('/index', [PersonaController::class,'index']);
+    Route::post('/add-persona', [PersonaController::class,'store']);
+    Route::get('/listar-persona', [PersonaController::class,'show']);
+    Route::put('/editar-persona/{id}', [PersonaController::class,'update']);
+    Route::delete('/eliminar-persona/{id}', [PersonaController::class,'destroy']);
